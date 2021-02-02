ecf1e9a5fc649ffdf32aff9338966e117f96965a
DevMethy <77387266+DevMethy@users.noreply.github.com>
2021-02-03 00:56:06 +0300

    Update Task4.java

--- Laba3/Task4.java
+++ Laba3/Task4.java
@@ -3,28 +3,28 @@ public class Main
 {
  public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
-      System.out.println(Введите число);
+      System.out.println("Введите число:");
       int num = input.nextInt();
       input.close();
       if (num=91 && num=100) {
-          System.out.println(A);
+          System.out.println("A");
       }
       else if (num=81 && num=90) {
-          System.out.println(B);
+          System.out.println("B");
       }
       else if (num=71 && num=80) {
-          System.out.println(C);
+          System.out.println("C");
       }
       else if (num=61 && num=70){
-          System.out.println(D);
+          System.out.println("D");
       }
       else if (num=51 && num=60){
-          System.out.println(E);
+          System.out.println("E");
       }
       else if (num=50){
-          System.out.println(F);
+          System.out.println("F");
       }
-      else System.out.println(Введите корректный бал от 1 до 100);
+      else System.out.println("Введите корректный бал от 1 до 100");
       
 	}
 }
